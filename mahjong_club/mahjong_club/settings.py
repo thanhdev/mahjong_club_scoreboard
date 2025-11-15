6d5f32cd0afa00c3dda03c8fee0febec10833f24
To Thanh <ThanhTo>
2025-11-15 17:14:43 +0700

    deploy

--- mahjong_club/mahjong_club/settings.py
+++ mahjong_club/mahjong_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0lzxa69=!%7g=-=^@-*tury80(h$$u7db#ewl%6hgagr%vgn5z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mahjongscoreboard.pythonanywhere.com"]
 
 
 # Application definition
